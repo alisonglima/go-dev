b15245d643d08b3c51be9dbff107bde7aeffc074
Alison Gustavo de Lima <alisonglima.dev@gmail.com>
2022-03-08 18:47:21 -0300

    First class files

---
+++ aulas/AULA_01/aula_01_02.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+// Incrementos, operadores lógicos
+
+let num1 = 10
+let num2 = 20
+
+let compairNumbers = num1 < num2
+
+print(compairNumbers)
+
+let name = "Alison"
+let lastName = "Lima"
+
+let compairStrings = name > lastName
+
+print(compairStrings)
+
+
+// Operadores
+/*
+  = Atribuição
+  == Igual a
+  != Diferente
+  > Maior que
+  < Menor que
+  <= Menor ou igual que
+  => Maior ou igual que
+*/
+
+
+// Expressões
+/*
+  ! Negação
+  || Ou
+  && E
+*/
+
+// Incremento
+/*
+  =+ Incremento
+  -= Decremento
+*/
